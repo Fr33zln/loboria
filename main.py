0dcff8888f9d271aef762e94061849984b69ad72
Fr33zln <fre3ezln@gmail.com>
2025-05-10 11:33:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from function import *
 
 
 window = pygame.display.set_mode(size_window)
-pygame.display.set_caption("MAZE")
+pygame.display.set_caption("Loboria")
 
 clock = pygame.time.Clock()
 
@@ -67,6 +67,17 @@ text_end = font.render("END", True, BLACK)
 text_levels = font.render("LEVELS", True, BLACK)
 
 
+#... = pygame.Rect(size_window[0]//2-x,y,width,height)
+lvl1 = pygame.Rect(size_window[0]//2-125,35,250,80)
+lvl2 = pygame.Rect(size_window[0]//2-125,145,250,80)
+lvl3 = pygame.Rect(size_window[0]//2-125,255,250,80)
+back = pygame.Rect(size_window[0]//2-125,365,250,80)
+
+text_LVL1 = font.render("LVL1", True, BLACK)
+text_LVL2 = font.render("LVL2", True, BLACK)
+text_LVL3 = font.render("LVL3", True, BLACK)
+text_back = font.render("BACK", True, BLACK)
+
 
 
 
@@ -94,12 +105,38 @@ while game:
                 x,y = event.pos
                 if rect_start.collidepoint(x,y):
                     what_window = "game"
+                if rect_levels.collidepoint(x,y):
+                    what_window = "levels"
                 elif rect_end.collidepoint(x,y):
                     game = False
+
+
     
+    elif what_window =="levels":
+        window.fill(BLACK)
+        pygame.draw.rect(window,WHITE, lvl1)
+        pygame.draw.rect(window,WHITE,lvl2)
+        pygame.draw.rect(window,WHITE,lvl3)
+        pygame.draw.rect(window,WHITE,back)
+        window.blit(text_LVL1, (lvl1.centerx - font.size("LVL1")[0] // 2,lvl1.centery - font.size("LVL1")[1]//2))
+        window.blit(text_LVL2, (lvl2.centerx - font.size("LVL2")[0] // 2,lvl2.centery - font.size("LVL2")[1]//2))
+        window.blit(text_LVL3, (lvl3.centerx - font.size("LVL3")[0] // 2,lvl3.centery - font.size("LVL3")[1]//2))
+        window.blit(text_back, (back.centerx - font.size("BACK")[0] // 2,back.centery - font.size("BACK")[1]//2))
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x,y = event.pos
+                if lvl1.collidepoint(x,y):
+                    what_window = "lvl1"
+                elif back.collidepoint(x,y):
+                    what_window = "menu"
 
 
-    elif what_window == "game":
+
+
+
+
+
+    elif what_window == "lvl1":
         window.fill(BLACK)
         window.blit(heart_image_list, (3,3))
         render_hp = font.render(f"x{hero.hp}", True, BLACK)
