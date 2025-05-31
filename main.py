0fd70727f27808b7d91bebfc9d6bb1c19ab293ba
Fr33zln <fre3ezln@gmail.com>
2025-05-31 11:37:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from function import *
 
-
 window = pygame.display.set_mode(size_window)
 pygame.display.set_caption("Loboria")
+
+
+camera_offset_x = 0
+camera_offset_y = 0
+
 
 clock = pygame.time.Clock()
 
@@ -22,7 +26,7 @@ hero = Hero(
 bot1 = Bot(
     100,
     335,
-    size_bot[0], # Use size_bot for bots
+    size_bot[0], 
     size_bot[1],
     bot1_image_list,
     1,
@@ -30,38 +34,45 @@ bot1 = Bot(
     radius=105,
 )
 
-
-bot4 = Bot(
-    900, 
-    10,
+bot2 = Bot(
+    745,
+    365,
     size_bot[0], 
     size_bot[1],
-    bot3_image_list,
+    bot1_image_list,
+    5,
+    "horizontal",
+    radius=215,
+)
+
+bot4 = Bot(
+    30, 
+    635,
+    size_bot[0], 
+    size_bot[1],
+    bot2_image_list,
     2,
-    "vertical",
-    radius=120,
+    "horizontal",
+    radius=150,
 )
 bullet4= Bullet(bot4.x+17,
                  bot4.y,
                  20, 20,
                  WHITE,
                  bot4.orientation,
-                 1)
+                 15)
 
 
 
+active_bots = [bot1,bot2, bot4]
 
 
-
-
-#heart_list.append(Heart(290,255,50, 50, heart_image_list))
 heart_list.append(Heart(390,80,50, 50, heart_image_list))
 
 font = pygame.font.Font(None, 40)
 what_window = "menu"
-rect_start = pygame.Rect(size_window[0]//2-125,200,250,80)
-rect_end = pygame.Rect(size_window[0]//2-125,350,250,80)
-rect_levels = pygame.Rect(size_window[0]//2-125,50,250,80)
+rect_end = pygame.Rect(size_window[0]//2-125,600,250,80)
+rect_levels = pygame.Rect(size_window[0]//2-125,150,250,80)
 text_start = font.render("START", True, BLACK)
 text_end = font.render("END", True, BLACK)
 text_levels = font.render("LEVELS", True, BLACK)
@@ -94,17 +105,14 @@ while game:
 
     if what_window == "menu":
         window.fill(BLACK)
-        pygame.draw.rect(window,WHITE, rect_start)
         pygame.draw.rect(window,WHITE,rect_end)
         pygame.draw.rect(window,WHITE,rect_levels)
-        window.blit(text_start, (rect_start.centerx - font.size("START")[0] // 2,rect_start.centery - font.size("START")[1]//2))
         window.blit(text_end, (rect_end.centerx - font.size("END")[0] // 2,rect_end.centery - font.size("END")[1]//2))
         window.blit(text_levels, (rect_levels.centerx - font.size("LEVELS")[0] // 2,rect_levels.centery - font.size("LEVELS")[1]//2))
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 x,y = event.pos
-                if rect_start.collidepoint(x,y):
-                    what_window = "game"
+
                 if rect_levels.collidepoint(x,y):
                     what_window = "levels"
                 elif rect_end.collidepoint(x,y):
@@ -131,85 +139,69 @@ while game:
                     what_window = "menu"
 
 
-
-
-
-
-
     elif what_window == "lvl1":
-        window.fill(BLACK)
-        window.blit(heart_image_list, (3,3))
+        window.fill(CYAN)
+        window.blit(heart_image_list, (3,3)) 
         render_hp = font.render(f"x{hero.hp}", True, BLACK)
-        window.blit(render_hp, (22,3))
-#    x,y = 0,0
-#    for i in range(100):
-#        pygame.draw.line(window, WHITE, (0, y), (size_window[0],y))
-#        pygame.draw.line(window, WHITE, (x, 0), (x, size_window[1]))
-#        x += 10
-#        y += 10
-        hero.grav(wall_list)
+        window.blit(render_hp, (22,3)) 
 
-        hero.move(window)
-        bot1.guardian(window)
-        bot4.striker(window, bullet4)
+        camera_offset_x, camera_offset_y, stomped_bot = hero.move(window, wall_list, camera_offset_x, camera_offset_y, active_bots) 
+
+        if stomped_bot:
+            if stomped_bot in active_bots:
+                active_bots.remove(stomped_bot) 
+                if stomped_bot == bot4: 
+                    bullet4.active = False 
+  
+        hero.collide_enemy(active_bots) 
+
+   
+        for bot in active_bots: #
+            if bot == bot1: 
+                bot.guardian(window, camera_offset_x, camera_offset_y)
+            elif bot == bot2: 
+                bot.guardian(window, camera_offset_x, camera_offset_y)
+            elif bot == bot4:
+                bot.striker(window, bullet4, camera_offset_x, camera_offset_y)
 
 
-
-        hero.collide_enemy([bot1,bot4,bullet4,])
-        hero.collide_heart(heart_list)
+        if bullet4.active:
+            bullet4.collide_hero(hero)
 
 
         for heart in heart_list:
-            heart.blit(window)
+            window.blit(heart.image, (heart.x - camera_offset_x, heart.y - camera_offset_y))
+            if heart.collide_hero(hero): 
+                heart_list.remove(heart) 
+                break 
+
 
         for wall_obj in wall_list:
-            window.blit(wall_obj.image, (wall_obj.x, wall_obj.y))
-
-
+            window.blit(wall_obj.image, (wall_obj.x - camera_offset_x, wall_obj.y - camera_offset_y))
 
         for event in events:
-
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and hero.can_jump:
                     hero.jump()
-
                 if event.key == pygame.K_d:
                     hero.walk["right"] = True
-                    
-          
                 if event.key == pygame.K_a:
                     hero.walk["left"] = True
 
-
-
             if event.type == pygame.KEYUP:
-                if event.type == pygame.K_SPACE:
+
+                if event.key == pygame.K_SPACE:
                     pass
                 if event.key == pygame.K_d:
                     hero.walk["right"] = False
                 if event.key == pygame.K_a:
-                    
                     hero.walk["left"] = False
 
     for event in events:
-            if event.type == pygame.QUIT:
-                game = False
+        if event.type == pygame.QUIT:
+            game = False
     clock.tick(FPS)
     pygame.display.flip()
 
-#for event in events:
-#        if event.type == pygame.QUIT:
-#            game = False
-#        if event.type == pygame.KEYDOWN:
-#            if event.key == pygame.K_a:
-#                hero.walk["left"] = True
-#            if event.key == pygame.K_d:
-#                hero.walk["right"] = True
-#            if event.key == pygame.K_SPACE:
-#                bullet_list_hero.append(Bullet(hero.centerx - 5,hero.y, 10, 20, RED, -5, image=None))
-#                hero.can_shoot = False
-#        if event.type == pygame.KEYUP:
-#            if event.key == pygame.K_a:
-#                hero.walk["left"] = False
-#            if event.key == pygame.K_d:
-#                hero.walk["right"] = False
+
+
